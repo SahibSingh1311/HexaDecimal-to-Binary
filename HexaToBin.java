2b88ffb6f78b55a3d2ad0834e351b5c4536241fe
Sahib Singh <51374188+SahibSingh1311@users.noreply.github.com>
2022-09-06 18:07:30 +0530

    Add files via upload

---
+++ HexaToBin.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class HexaToBin{
+public static void main(String[] args){
+	String s1 = "4F6";
+	
+	toBinary(s1);
+}
+static int toDeci(String inp){
+	int j=0;	//power 
+	int out=0;
+	for(int i=inp.length()-1;i>=0;i--){					//Reverse traversing of String so as to obtain characters from end.
+	  		if(inp.charAt(i)>='0' && inp.charAt(i)<='9'){		//If character is between 0 & 9
+				out += (inp.charAt(i)-48)*(Math.pow(16,j));  	//'-48' bcs its the ASCII code of 0
+				j++;						//increment of power
+			}
+			else if(inp.charAt(i)>='A'&&inp.charAt(i)<='F'){	//If character is between 'A' & 'F'
+				out += (inp.charAt(i)-55)*(Math.pow(16,j)); 	//'-55' bcs ASCII code of 'A' is 65 so to obtain 10 we will subtract it with 55
+				j++;						//increment of power
+			}
+	}
+	System.out.println("Decimal Conversion is: "+out);
+	return out;
+}
+static void toBinary(String inp){
+	int dec = toDeci(inp);
+	int[] binaryNo= new int[35];
+	int id=0; 
+		
+	while(dec>0){
+	binaryNo[id++] = dec%2;
+	dec /=2;
+	}
+	for(int i=id-1;i>=0;i--)			//setting i=id-1 bcs its the index at which the last binary number is stored.
+		System.out.print(binaryNo[i]);		//Reverse Printing of Array.
+}
+
+}
