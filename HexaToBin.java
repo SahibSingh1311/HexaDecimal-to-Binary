af82e11d223b85b6fb7e0373c010306322058164
Sahib Singh <51374188+SahibSingh1311@users.noreply.github.com>
2022-09-07 11:57:06 +0530

    Update HexaToBin.java

--- HexaToBin.java
+++ HexaToBin.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class HexaToBin{
 public static void main(String[] args){
-	String s1 = "4F6";
+	String s1 = "AG9";
 	
 	toBinary(s1);
 }
@@ -12,14 +12,19 @@ static int toDeci(String inp){
 	for(int i=inp.length()-1;i>=0;i--){					//Reverse traversing of String so as to obtain characters from end.
 	  		if(inp.charAt(i)>='0' && inp.charAt(i)<='9'){		//If character is between 0 & 9
 				out += (inp.charAt(i)-48)*(Math.pow(16,j));  	//'-48' bcs its the ASCII code of 0
-				j++;						//increment of power
+										//increment of power
 			}
 			else if(inp.charAt(i)>='A'&&inp.charAt(i)<='F'){	//If character is between 'A' & 'F'
 				out += (inp.charAt(i)-55)*(Math.pow(16,j)); 	//'-55' bcs ASCII code of 'A' is 65 so to obtain 10 we will subtract it with 55
-				j++;						//increment of power
+										//increment of power
 			}
+			else{
+				System.out.println("Incorrect HexaDecimal Number"); //If incorrect HexaDecimal Number is passed	
+				return 0;					// Return 0
+			}
+			j++;
 	}
-	//System.out.println("Decimal Conversion is: "+out);
+	System.out.println("Decimal Conversion is: "+out);
 	return out;
 }
 static void toBinary(String inp){
